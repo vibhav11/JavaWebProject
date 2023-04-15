b722b0ab4df75540edc44621bc26363fe7325280
unknown <jain.vibhav017@gmail.com>
2023-04-16 01:43:10 +0530

    first commit from git bash

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ package vibhav.bank.test;
 
 import java.io.IOException;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -15,27 +16,24 @@ import java.util.Scanner;
 public class HelloWorld {
 
 	/**
-	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String[] bankingMenu = {"1. Open Bank Account","2. Close Bank Account","3. Deposite Money","4. Withdraw money","5. Check Balance","6. Bank executive portal(4 digit Employee code is mandatory)"};
+		String[] bankingMenu = {"1. Open Bank Account","2. Close Bank Account","3. Deposit Money","4. Withdraw money","5. Check Balance","6. Bank executive portal(4 digit Employee code is mandatory)"};
 		System.out.println("Welcome to \"State Bank of Noida\"\nChoose from the options below:");
-			
-		for(int i=0; i< bankingMenu.length;i++) {
-			System.out.println(bankingMenu[i]);
-		} 	
-		//HelloWorld obj=new HelloWorld();
-		
-		//int option1;
-		
-		try{		
+
+		for (String menu : bankingMenu) {
+			System.out.println(menu);
+		}
+
+		try{
+			System.out.println();
 		int option1= getInput();
 		System.out.println("You selected :" + bankingMenu[option1-1]);		
 		}
 		catch(IOException e) {
-			System.out.println(e.getStackTrace());
+			System.out.println(Arrays.toString(e.getStackTrace()));
 		}
 		
 		
@@ -48,9 +46,8 @@ public class HelloWorld {
 			 * InputStreamReader(System.in));
 			 */      
 		Scanner s = new Scanner(System.in);
-			
-		int option2=s.nextInt();
-        return option2;
+
+		return s.nextInt();
 	}
 	
 	
